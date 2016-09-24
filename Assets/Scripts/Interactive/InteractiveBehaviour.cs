68a113e59d47c235da19e9b817e0a7f07b1d62d3
cesarpeixoto <cesars_peixoto@hotmail.com>
2016-09-24 13:25:41 -0300

    .

---
+++ Assets/Scripts/Interactive/InteractiveBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class InteractiveBehaviour : MonoBehaviour
+{
+
+    public abstract void SetInteractive();
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
