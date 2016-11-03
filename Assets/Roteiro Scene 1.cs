a9b11d41cfda3cc17c2e17d3621d7c59b54ca003
rodolfogentil <rodolfo.gentil@gmail.com>
2016-11-03 18:27:53 -0200

    Adicionando script da scene 1

---
+++ Assets/Roteiro Scene 1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoteiroScene1 : MonoBehaviour 
+{
+	/*o char começa onde ele esta mesmo, nessa sala ele tem a chave da porta e o iniciador do tempo de respiração, colocar em cima do palete, sem o repsirador o personagem nao pode sair da sala.
+	saindo para a sala maior ele tera que arrastar a caisa grande até o armario, em cima doarmario esta a bateria do elevador da sala inicial, com isso
+	ele podera ir pro andar de cima, empurrar o pallete com caixa até ele cair, ao cair ele dara lugar a uma semente
+	ele deve subir a corda e plntar a semente para salvar o status e liberar a chave da principal.*/
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
