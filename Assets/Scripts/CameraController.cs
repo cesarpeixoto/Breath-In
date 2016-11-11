c987a6ccaf6234f6e3097dc3fc3f6a239b21ebd9
lucas1295santos <lucas1295santos@gmail.com>
2016-11-11 17:59:10 -0200

    Efeito de camera v1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour 
 {
 	public Transform target;								// Referência para o GameObject que será seguido.
 	public float movimentSpeed = 0.1f;						// Velocidade do movimento da Camera.
     public float distance = 12f;
+    Transform _transform;
+
+    void Start()
+    {
+        _transform = transform;
+    }
 
 	// Update is called once per frame
 	void FixedUpdate () 
@@ -14,7 +20,9 @@ public class CameraController : MonoBehaviour
 		{
             Vector3 destination = new Vector3(target.position.x - distance, target.position.y +3f, 		// Cria um vetor para posição de destino, mantendo o eixo z inalterado.
                                                 target.position.z);
-			transform.position = Vector3.Lerp (transform.position, destination, movimentSpeed);	// Faz o movimento da Camera seguir para nova posição com fluidez, na velocidade de lerpSpeed.
+            _transform.position = Vector3.Lerp (transform.position, destination, movimentSpeed);	// Faz o movimento da Camera seguir para nova posição com fluidez, na velocidade de lerpSpeed.
 		}
+
+        
 	}
 }
